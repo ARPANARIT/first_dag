f04a8feefe699295ae809d7e53f98abd8d481abb
ARPANARIT <arpanarit08@gmail.com>
2024-07-21 01:21:01 +0530

    days_ago func

--- airflow/dags/test_dag.py
+++ airflow/dags/test_dag.py
@@ -1,19 +1,25 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-
+from airflow.utils.dates import days_ago
 
 def print_test():
     print('This dag is to text git integration !')
 
 dag = DAG(
+
     'test_dag',
-    schedule_interval='0 12 * * *',
+
+    default_args={'start_date': days_ago(1)},
+
+    schedule_interval='2 * * * *',
+
     catchup=False
+
 )
 
 only_task = PythonOperator(
-    task_id='print_test',
-    python_callable=print_test,
+    task_id ='print_test',
+    python_callable = print_test,
     dag = dag
 )
 
